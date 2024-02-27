faf0ccbb9113f6d67550641f5ee10324d17959f2
Dmytro Ganaga <dimon14fcmk@gmail.com>
2024-02-28 01:47:31 +0200

    Changed categories value

--- app/com/ganaga/reviews/store/CategoriesStore.scala
+++ app/com/ganaga/reviews/store/CategoriesStore.scala
@@ -5,7 +5,7 @@ import akka.stream.scaladsl.Source
 
 object CategoriesStore {
 
-  val categoriesNames: List[String] = List("Electronics Store", "Jewelry Store", "Clothing Store", "Computer Store")
+  val categoriesNames: List[String] = List("Electronics Store", "Jewelry Store", "Clothing Store")
 
   val categoriesIds: List[String] = categoriesNames.map(s => s.toLowerCase.replace(" ", "_"))
 
