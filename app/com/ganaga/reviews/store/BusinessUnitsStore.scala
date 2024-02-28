bef5222d07755ca964aac8aa89de4fa6c6d44597
Dmytro Ganaga <dimon14fcmk@gmail.com>
2024-02-28 02:04:46 +0200

    Removed unused variable

--- app/com/ganaga/reviews/store/BusinessUnitsStore.scala
+++ app/com/ganaga/reviews/store/BusinessUnitsStore.scala
@@ -1,12 +1,9 @@
 package com.ganaga.reviews.store
 
-import akka.stream.scaladsl.Source
 import com.ganaga.reviews.model.BusinessUnitEntity
 
 object BusinessUnitsStore {
   private var businessUnits: Map[String, BusinessUnitEntity] = Map()
-
-  val businessUnitsSource = Source.fromIterator(() => businessUnits.valuesIterator)
 
   def saveBusinessUnit(businessUnit: BusinessUnitEntity): Map[String, BusinessUnitEntity] = {
     businessUnits = businessUnits + (businessUnit.businessUnitId -> businessUnit)
